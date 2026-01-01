8f52606d8e271ea74d0270b23ef906e9a6ec5cef
NaveedAhmed286 <00305cheema@gmail.com>
2026-01-01 20:35:47 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Start the FastAPI app
 PORT=${PORT:-8000}
 echo "Starting Amazon Scraper System on port $PORT"
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
